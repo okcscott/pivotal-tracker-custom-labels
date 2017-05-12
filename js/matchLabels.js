be6abdbb8e97c8546f0da59cf76c1b0159697089
Dstrasel <siruku@gmail.com>
2017-05-12 15:05:59 -0400

    Add missing semi-colons.

--- js/matchLabels.js
+++ js/matchLabels.js
@@ -29,21 +29,21 @@ chrome.extension.sendMessage({}, function(response) {
       var handleMutationEvents = function handleMutationEvents(mutation) {
         Array.prototype.forEach.call(mutation.addedNodes, styleLabelsInNode);
         styleLabelsInNode(mutation.target);
-      }
+      };
 
       var styleLabelsInNode = function styleLabelsInNode(node) {
         if (nodeIsElement(node)) {
           styleLabels(findLabelsInNode(node));
         }
-      }
+      };
 
       var nodeIsElement = function nodeIsElement(node) {
         return (typeof node.querySelectorAll !== 'undefined');
-      }
+      };
 
       var findLabelsInNode = function findLabelsInNode(node) {
         return node.querySelectorAll('a.label');
-      }
+      };
 
       var styleLabels = function styleLabels(labels) {
         Array.prototype.forEach.call(labels, function(label) {
